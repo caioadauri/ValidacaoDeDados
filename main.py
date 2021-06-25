51e8b67d473d2a64dadb142b2f92ef8e74b345ef
Caio Adauri <caioadaurigomes@gmail.com>
2021-06-25 10:12:30 -0300

    validando cnpj

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from validate_docbr import CPF
-from Cpf import Cpf
+from cpf_cnpj import CpfCnpj
+from validate_docbr import CNPJ
 
-cpf = CPF()
-print(cpf.validate("01234567890"))
+#cpf_um = Cpf("15316264754")
+#print(cpf_um)
 
+exemplo_cnpj = "35379838000112"
+exemplo_cpf = "32007832062"
+# cnpj = CNPJ()
+# print(cnpj.validate(exemplo_cnpj))
 
+documento = CpfCnpj(exemplo_cnpj, 'cnpj')
+documento2 = CpfCnpj(exemplo_cpf, "cpf")
+print(documento)
+print(documento2)
 
